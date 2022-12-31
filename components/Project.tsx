d226d0193239faa28ce7159c65747cbed93e25b1
AttahSosah <sosahattah55@gmail.com>
2022-12-31 19:50:15 +0300

    attempt 5 at deployment

--- components/Project.tsx
+++ components/Project.tsx
@@ -14,7 +14,7 @@ function Project({image,title,link,description,number}: Props) {
     const projects = [1,2]
   return (
     <div>
-        {projects.map((key,i) => (
+        {projects.map((project,i) => (
                 <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'> 
                     <motion.img 
                     className='max-h-56 max-w-56'
