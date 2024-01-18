e17f6134cea791d6c20a2305a3cac97d3483d59b
Attahsosah <sosahattah55@gmail.com>
2024-01-18 10:15:04 +0200

    changes to skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -21,15 +21,11 @@ function Skill({directionLeft, image, proficiency}: Props) {
             transition={{ duration:1}}
             whileInView={{ opacity:1, x:0}}
             src={image}
-            className='rounded-full sm:top-0   border border-gray-500 object-cover h-24 w-24 md:w-28 md:h-28  xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+            className='rounded-full sm:top-0   border border-gray-500 hover:scale-150 object-cover h-24 w-24 md:w-28 md:h-28  xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
         
         
         />
-        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:h-32 xl:w-32 rounded-full z-0'>
-            <div className='flex items-center justify-center h-full'>
-                <p className='text-3xl font-bold text-black opacity-100'>{proficiency}%</p>
-            </div>
-        </div>
+      
         
 
         
